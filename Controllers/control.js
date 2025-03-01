345bfee09fded6f7e6a15a6ce75099be9036782c
Saitej18e8e <saitejsunilwadekar@gmail.com>
2025-03-02 00:53:40 +0530

    backend commit

---
+++ Controllers/control.js
@@ -0,0 +1,43 @@
+import bcrypt from "bcryptjs";
+import json from "jsonwebtoken";
+import {User} from "../Models/User.js";
+
+export const register = async (req,res)=>{
+    const  {name, gmail, password } = req.body;
+
+    try{
+    let user = await User.findOne({gmail});
+    if(user) return res.json({message:"User Already exist"});
+//here i use hashpass objet to assign hash password with 10 salt round where both user have same password have unique hash key
+    const hashpass = await bcrypt.hash(password,10)
+    user = await User.create({name, gmail, password:hashpass});
+    res.json({message:`Welcome! Let’s get started! ${user.name}`});
+    }catch(err){
+        res.json({message:Error})
+    }
+}
+
+export const login = async(req, res)=>{
+    const {gmail, password}=req.body;
+try{
+   let user = await User.findOne({gmail});
+   if(!user) return res.json({message:"User not exited...."});
+
+   const validPass = await bcrypt.compare(password,user.password);
+
+   if(!validPass) return res.json({message:"oops Inavlid Crediatials"});
+
+   //here i created webtoken to secure more and user not want login  when visot website
+   const token = json.sign({userid:user.id}, "!@#$%^&*()",{
+  expiresIn : "2h"
+   });
+
+   res.json({message:`Welcome to your dashboard! ${user.name}`, token})
+}catch(err){
+    res.json({message:Error});
+}
+}
+
+export const profile = async (req,res)=>{
+    res.json({user:req.user})
+}
---
+++ Router/Router.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { register, login,profile } from "../Controllers/control.js";
+import { Authenticate } from "../Middleware/auth.js";
+
+const router = express.Router();
+
+router.post("/register",register );
+
+router.post("/login",login);
+
+// for profile
+router.get("/user",Authenticate,profile)
+
+export default router;
